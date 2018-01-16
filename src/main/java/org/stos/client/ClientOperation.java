8f6f0cffdf5fb1ae087199745a2f3a8aa1ea08d4
Stuart Osborne <stuart.osborne@infinera.com>
2018-01-16 16:08:51 +0100

    Changed the server to use its local registry and the client to then look it up.

--- src/main/java/org/stos/client/ClientOperation.java
+++ src/main/java/org/stos/client/ClientOperation.java
@@ -3,15 +3,18 @@ package org.stos.client;
 import org.stos.rmi.RMIInterface;
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class ClientOperation {
     private static RMIInterface lookUp;
 
     ClientOperation() throws RemoteException, NotBoundException, MalformedURLException {
-        lookUp = (RMIInterface) Naming.lookup("//localhost/TheServer");
+        Registry registry = LocateRegistry.getRegistry(1099);
+        lookUp = (RMIInterface) registry.lookup("//localhost/TheServer");
+//        lookUp = (RMIInterface) Naming.lookup("//localhost/TheServer");
     }
 
     public RMIInterface lookUpServer() {
--- src/main/java/org/stos/server/MainServerApp.java
+++ src/main/java/org/stos/server/MainServerApp.java
@@ -1,20 +1,20 @@
 package org.stos.server;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 
 public class MainServerApp {
 
     public static void main(String[] args) {
 
         try {
-            RMIRegistryFactory.createRmiRegistry(1099);
-            Naming.rebind("//localhost/TheServer", new ServerOperation());
+            Registry rmiRegistry = RMIRegistryFactory.createRmiRegistry(1099);
+            rmiRegistry.rebind("//localhost/TheServer", new ServerOperation());
+            //Naming.rebind("//localhost/TheServer", new ServerOperation());
         } catch (RemoteException e) {
             System.out.println("remote exeption thrown with reason: " + e.getMessage());
-        } catch (MalformedURLException e) {
-            System.out.println("malformed url exception thrown with reason: " + e.getMessage());
+        /*} catch (MalformedURLException e) {
+            System.out.println("malformed url exception thrown with reason: " + e.getMessage());*/
         }
         System.err.println("Server ready");
     }
